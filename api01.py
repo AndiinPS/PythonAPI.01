30be1a5f7df1ca8a110036c9457d011667686ee8
AndiinPS <andiinps@gmail.com>
2023-11-09 21:31:24 -0300

    aula

--- api01.py
+++ api01.py
@@ -1,4 +1,4 @@
-
+# Importa a biblioteca 'json'.
 import json
 
 
@@ -22,17 +22,28 @@ items = [
 
 
 ]
-
+# Função que lê e lista todos os itens da coleção.
 def get_all():
-    var_json=json.dumps(items, indent=2)
-    print(var_json)
     
-# get_all()  
-
-def get_one(id):
-    var_json=json.dumps(items[id], indent=2)  
+    # Converte a lista 'items' para json e armazena em 'var_json'
+    var_json=json.dumps(items, indent=2)
+    
+     # Imprime o json.
     print(var_json)
 
+# Chama (call) a função get_all().    
+get_all()  
+
+# Função que lê um item específico, identificado pelo índice.
+def get_one(id):
+    
+     # Converte o dicionario 'items[id]' para json e armazena em 'var_json'
+    var_json=json.dumps(items[id], indent=2) 
+     
+     # Imprime o json.
+    print(var_json)
+
+# Chama a função get_one(), passando o índice como parâmetro.
 get_one(0)    
     
 
